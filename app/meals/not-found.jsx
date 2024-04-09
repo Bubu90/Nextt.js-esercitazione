bb1de2f5a7c7854a53c405db83375042c6d0e38e
Bubu90 <brunolaria1390gmail.com>
2024-04-09 17:45:00 +0200

    error sate

---
+++ app/meals/not-found.jsx
@@ -0,0 +1,8 @@
+export default function notFound() {
+  return (
+    <main className="not-found">
+      <h1>Meal Not Found</h1>
+      <p>Unfortunately, that meal does not exist. </p>
+    </main>
+  );
+}
